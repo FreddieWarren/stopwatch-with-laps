a4b726c4edab274c6ffc57320e9c463e7ccfab19
freddiewarren <fredswow@live.co.uk>
2020-07-13 15:58:06 +0100

    stopwatch components done and added to app.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Stopwatch } from './stopwatch';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <Stopwatch />
     </div>
   );
+}
 }
 
 export default App;
